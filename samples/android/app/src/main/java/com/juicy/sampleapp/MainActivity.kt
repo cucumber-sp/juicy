2623a381c3392b8e6ea5c0c5fbd24ac7a912aa0e
Andrey Onishchenko <loraner123@gmail.com>
2024-02-23 21:05:14 +0300

    Android sample app

---
+++ samples/android/app/src/main/java/com/juicy/sampleapp/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.juicy.sampleapp
+
+import android.os.Bundle
+import com.juicy.lib.JuicyActivity
+
+class MainActivity : JuicyActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        openUrl("https://google.com")
+    }
+}
---
+++ samples/android/settings.gradle.kts
@@ -0,0 +1,20 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Juicy Sample App"
+include(":app")
+
+include(":lib")
+project(":lib").projectDir = File("../../android/lib")
